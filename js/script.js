4d0ce6e35ae640f23d208f6ec08073f1d19a4519
Sergio Henrique <sergimmatos@hotmail.com>
2025-08-26 19:17:34 -0300

    function to display the top rated movies created

--- js/script.js
+++ js/script.js
@@ -1,7 +1,68 @@
-
-const global = { 
-    currentPage: window.location.pathname
+const global = {
+  currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+    totalResults: 0,
+  },
+  api: {
+    // Register your key at https://www.themoviedb.org/settings/api and enter here
+    // Only use this for development or very small projects. You should store your key and make requests from a server
+    apiKey: 'e168671b32408f28473fafdcc8b8460a',
+    apiUrl: 'https://api.themoviedb.org/3/',
+  },
 };
+
+async function displayPopularMovies() {
+  const { results } = await fetchAPIdataFromTMDB('movie/top_rated');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+            <p class="card-text">
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+          </div>
+        `;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+async function fetchAPIdataFromTMDB(endpoint) {
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl;
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+    const data = await response.json();
+    
+    return data;
+}
 
 function hightlightActiveLink() {
     const links = document.querySelectorAll('.nav-link');
@@ -16,7 +77,7 @@ function initApp() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
-            console.log('"HOME PAGE"');
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log('"SHOWS PAGE"');
